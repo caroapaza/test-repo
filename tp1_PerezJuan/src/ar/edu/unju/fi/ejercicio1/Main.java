a0e50643a9e1e39911fb3c992003d04bacba6c7b
caroapaza <apazacarolina@gmail.com>
2024-04-08 19:04:40 -0300

    Primer commit, creación de paquete y clase para ejercicio1

---
+++ tp1_PerezJuan/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// desarrollo de ejercicio 1
+		System.out.println("Ejercicio 1");
+
+	}
+
+}
